d119bac6ab5d76b906fba9b3e065d6e4a4fd6004
sinxocde <sinarahimi3295@gmail.com>
2020-12-12 13:06:31 +0330

    dropdown

--- logic.js
+++ logic.js
@@ -1,6 +1,8 @@
 var bars = document.getElementsByClassName("bar");
 var menu = document.getElementsByClassName("hamburgur-menu");
 
+var dropDown = document.getElementById("dropdown");
+
 
 
 function barClicked() {
@@ -14,6 +16,23 @@ function barClicked() {
     bars[2].classList.toggle("bar3-clicked");
 
     menu[0].classList.toggle("hamburgurMenu-clicked");
+
+
+
+    if (dropDown.style.display == "block") {
+
+        dropDown.style.display = "none";
+        dropDown.style.height = "0";
+        document.getElementsByTagName("body")[0].style.overflow = "visible";
+    } else {
+
+        dropDown.style.display = "block";
+
+        dropDown.style.height = "100vh";
+        document.getElementsByTagName("body")[0].style.overflow = "hidden";
+
+    }
+
 
 
 
